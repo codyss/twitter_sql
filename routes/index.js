648f821f9da58b682ecb8a0110b4eb2f11358894
codyss <cody.schwarz@gmail.com>
2016-01-25 15:01:43 -0500

    end of lab

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 var tweetBank = require('../tweetBank');
 var tweetModels = require('../models/');
+var sequelize = require('sequelize');
 
 module.exports = function makeRouterWithSockets (io) {
 
@@ -29,7 +30,6 @@ module.exports = function makeRouterWithSockets (io) {
       })
       return cleanTweets;
     }).then(function(data) {
-      console.log(data);
       res.render('index', {
       title: 'Twitter.js',
       tweets: data,
@@ -48,17 +48,23 @@ module.exports = function makeRouterWithSockets (io) {
       include: [
       {model: tweetModels.Tweet, required: true}
       ]
-    }).then(function(data) {
-        var tweetsWanted = data.map(function(each) {
-          return {name: each.dataValues.name, text: each.dataValues["Tweets"][0].dataValues.tweet, id: each.dataValues.id};
-        })
-        return tweetsWanted.filter(function(each) {
-          return each.name === userName;
-        })
-      }).then(function(data) {
+      })
+    .then(function(data) {
+      //Returns the one user we are interested in
+        var userArray = data.filter(function(each) {
+          return each.dataValues.name === userName;
+        });
+        var tweetArray = []
+        userArray[0].dataValues.Tweets.forEach(function(tweetText) {
+          tweetArray.push({name: userArray[0].dataValues.name, text: tweetText.dataValues.tweet, id: userArray[0].dataValues.id});
+        });
+      return tweetArray;
+      }
+    )
+    .then(function(data) {
         res.render('index', {
           title: 'Twitter.js',
-          tweets: data,
+          tweets: data, //array of tweet objects
           showForm: true,
           username: userName
         });
@@ -98,10 +104,37 @@ module.exports = function makeRouterWithSockets (io) {
 
   // create a new tweet
   router.post('/tweets', function(req, res, next){
-    var newTweet = tweetBank.add(req.body.name, req.body.text);
-    io.sockets.emit('new_tweet', newTweet);
-    res.redirect('/');
+    var user = req.body.name;
+    var tweetToPost = req.body.text;
+    //SEARCH IF USER EXITS
+    tweetModels.User.findAll({where:{'name':user}})
+    .then(function (data) {
+      if (data.length > 0) {
+    //IF USER - POST TWEET AS THAT USER
+        var userId = data[0].dataValues.id;
+        tweetModels.Tweet.create({tweet: tweetToPost, UserId: userId});
+
+      } else {
+    //IF NO USER - CREATE USER AND POST TWEET AS THAT USER
+        tweetModels.User.create({name: user, pictureUrl: "http://www.fullstackacademy.com/img/team/zeke_nierenberg@2x_nerd.jpg"})
+        .then(function() {
+          var newUserId = tweetModels.User.count(); return newUserId})
+        .then(function(data) {
+          tweetModels.Tweet.create({tweet: tweetToPost, UserId: data});          
+        });
+        // tweetModels.User.findAll({attributes: sequelize.fn('COUNT', sequelize.col('id')), 'num_users'})
+      }
+    }).then(
+    function(tweet) {
+      io.sockets.emit('new_tweet', tweet);
+      res.redirect('/');
+    });
   });
+
+
+// Tweet.create({ title: 'foo', description: 'bar', deadline: new Date() }).then(function(task) {
+//   // you can now access the newly created task via the variable task
+// })
 
   // // replaced this hard-coded route with general static routing in app.js
   // router.get('/stylesheets/style.css', function(req, res, next){
